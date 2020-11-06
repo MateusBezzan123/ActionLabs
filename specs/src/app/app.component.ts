3a9bc1fb283b0e5fcbb9cf6c1b4b6b3b399f7777
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-11-06 09:53:09 -0300

    ADD projeto

--- specs/src/app/app.component.ts
+++ specs/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=76d1b43ba3695cfae59aa9f7dc9b4877';
 export class AppComponent {
   title = 'specs';
+  constructor(private http: HttpClient) { }
 }
--- specs/src/app/app.module.ts
+++ specs/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgMatSearchBarModule } from 'ng-mat-search-bar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    NgMatSearchBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
